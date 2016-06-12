2221826afa17bf219ffa979cc07579402fed25e7
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-12 01:40:05 -0700

    Update wtnDice.py dependency

--- wtnDice.py
+++ wtnDice.py
@@ -1,4 +1,4 @@
-#python port of whtnDice:
+#python port of wtnDice:
 
 #import neccesary libraries
 from datetime import datetime
@@ -26,19 +26,20 @@ def rolDBize(di, rolDB):
     rolNtry = [di['time'],di['nmbr'],di['sids'],di['rols'],di['totl']]
     rolDB.append(rolNtry)
 
+#Not used except in CLI
 def whtnize(di):
     if di['totl'] == di['nmbr']:
 	di['whtn'] = ' Wheaton!'
     elif di['totl'] == di['nmbr'] * di['sids']:
 	di['whtn'] = ' Percy!'
     elif di['totl'] < (di['nmbr'] * di['sids'])/3:
-	di['whtn'] = ' lesser wheaton.'
+	di['whtn'] = ' lesser Wheaton.'
     elif di['totl'] > ((di['nmbr'] * di['sids'])/3)*2:
 	di['whtn'] = ' lesser Percy.'
     else: 
 	di['whtn'] = ''
 
-#Print function not used when wtnDice not used as main. 
+#Not used except in CLI
 def rollOut(di):
     return "%s d%s: %s Total: %s%s" % (di['nmbr'], di['sids'],
 	di['rols'], di['totl'], di['whtn'])
@@ -51,8 +52,8 @@ def roll(dNum,dSid,di,rolDB):
     physics(di)
     rolDBize(di, rolDB)
     whtnize(di)
+    #return rollOut(di) #For CLI
     return di['totl']
-
 
 #def rollMix():
 
@@ -69,6 +70,7 @@ def rollHigher(dNum,dSid,di,rolDB):
     di['nmbr'] = 'H'
     rolDBize(di, rolDB)
 
+    #return rollOut(di) #For CLI
     return di['totl']
 
 def rollLower(dNum,dSid,di,rolDB):
@@ -84,6 +86,7 @@ def rollLower(dNum,dSid,di,rolDB):
     di['nmbr'] = 'L'
     rolDBize(di, rolDB)
 
+    #return rollOut(di) #for CLI
     return di['totl']
 
 def rollDigit(dNum,dSid,di,rolDB):
@@ -109,15 +112,52 @@ def rollDigit(dNum,dSid,di,rolDB):
     whtnize(di)
     di['nmbr'] = 'D'
 
+    #return rollOut(di) #For CLI
     return di['totl']
 
-#def rollExtra():
+def rollExtra(dNum,dSid,di,rolDB):
+    lastRoll = di['totl']
+    di['nmbr'] = dNum
+    di['sids'] = dSid
+    physics(di)
+    whtnize(di)
+    di['totl'] += lastRoll
+    di['nmbr'] = 'X'
+    rolDBize(di,rolDB)
+    #return rollOut(di) #For CLI
+    return di['totl']    
 
-#def rollManual():
+#Do I need dNum arg if i'm just going to change it to 'M'?
+def rollManual(dNum,dSid,rolList,di,rolDB):
+    di['nmbr'] = dNum
+    di['sids'] = dSid
+    di['rols'] = rolList
+    di['totl'] = sum(rolList)
+    rolDBize(di,rolDB)
+    whtnize(di)
+    #return rollOut(di) #For CLI
+    return di['totl']
+
 
 def main():
     di = {'time':'','nmbr':0,'sids':0,'rols':[],'totl':0,'whtn':''}
     rolDB = []
+
+    #Is there some way to limit the args to two? 4 is weird.
+    print roll(1,20,di,rolDB)
+    print rollHigher(2,20,di,rolDB)
+    print rollLower(2,20,di,rolDB)
+    print rollExtra(1,10,di,rolDB)
+    print rollDigit(2,10,di,rolDB)
+    #print rollDigit(4,10,di,rolDB)
+    #print rollDigit(5,2,di,rolDB)
+    #print rollDigit(2,4,di,rolDB)
+    #print rollDigit(2,8,di,rolDB)
+    #print rollDigit(2,12,di,rolDB)
+    #print rollDigit(2,20,di,rolDB)
+    print rollManual(1,20,[20],di,rolDB)
+    print rollManual(3,12,[12,12,12],di,rolDB)
+    print rolDB
 
 if __name__ == '__main__':
     main()
